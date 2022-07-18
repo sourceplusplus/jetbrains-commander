26c84f4669a49db88faa0e2c97b3466b5e4bcb92
Brandon Fergerson <bfergerson@apache.org>
2022-07-18 16:51:15 +0400

    refactor live commands/indicators as live plugins

---
+++ resources/.spp/plugins-experimental/endpoint-load/plugin.kts
@@ -0,0 +1,63 @@
+import com.apollographql.apollo3.api.Optional
+import com.intellij.openapi.application.ApplicationManager
+import io.vertx.core.json.JsonObject
+import monitor.skywalking.protocol.type.Order
+import monitor.skywalking.protocol.type.Scope
+import monitor.skywalking.protocol.type.TopNCondition
+import spp.indicator.LiveIndicator
+import spp.jetbrains.marker.impl.ArtifactCreationService
+import spp.jetbrains.marker.source.info.EndpointDetector
+import spp.jetbrains.marker.source.mark.api.MethodSourceMark
+import spp.jetbrains.marker.source.mark.api.event.SourceMarkEvent
+import spp.jetbrains.marker.source.mark.api.event.SourceMarkEventCode.MARK_USER_DATA_UPDATED
+import spp.jetbrains.marker.source.mark.guide.GuideMark
+import spp.jetbrains.monitor.skywalking.SkywalkingClient.DurationStep
+import spp.jetbrains.monitor.skywalking.model.ZonedDuration
+import spp.plugin.*
+import spp.plugin.registerIndicator
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+
+class EndpointLoadIndicator : LiveIndicator() {
+    override val listenForEvents = listOf(MARK_USER_DATA_UPDATED)
+
+    override suspend fun trigger(guideMark: GuideMark, event: SourceMarkEvent) {
+        val endpointName = guideMark.getUserData(EndpointDetector.ENDPOINT_NAME) ?: return
+        if (EndpointDetector.ENDPOINT_NAME != event.params.firstOrNull()) return
+        val highLoadEndpoints = getHighLoadEndpoints()
+
+        if (highLoadEndpoints.contains(endpointName)) {
+            ApplicationManager.getApplication().runReadAction {
+                val gutterMark = ArtifactCreationService.createMethodGutterMark(
+                    guideMark.sourceFileMarker,
+                    (guideMark as MethodSourceMark).getPsiElement().nameIdentifier!!,
+                    false
+                )
+                gutterMark.configuration.activateOnMouseHover = false //todo: show tooltip with extra info
+                gutterMark.configuration.icon = findIcon("endpoint-load/icons/high-load-icon.svg")
+                gutterMark.apply(true)
+            }
+        }
+    }
+
+    private suspend fun getHighLoadEndpoints(): List<String> {
+        val endTime = ZonedDateTime.now().minusMinutes(1).truncatedTo(ChronoUnit.MINUTES) //exclusive
+        val startTime = endTime.minusMinutes(15)
+        val duration = ZonedDuration(startTime, endTime, DurationStep.MINUTE)
+        val serviceId = skywalkingMonitorService.getCurrentService().name
+        val highLoadEndpoints = skywalkingMonitorService.sortMetrics(
+            TopNCondition(
+                "endpoint_cpm",
+                Optional.presentIfNotNull(serviceId),
+                Optional.presentIfNotNull(true),
+                Optional.presentIfNotNull(Scope.Endpoint),
+                (skywalkingMonitorService.getEndpoints(serviceId, 1000).size() * 0.20).toInt(), //relative top 20%
+                Order.DES
+            ), duration
+        )
+
+        return highLoadEndpoints.map { (it as JsonObject).getString("name") }
+    }
+}
+
+registerIndicator(EndpointLoadIndicator())
---
+++ resources/.spp/plugins-experimental/last-log/plugin.kts
@@ -0,0 +1,107 @@
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.ui.JBColor
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
+import spp.indicator.LiveIndicator
+import spp.jetbrains.marker.impl.ArtifactCreationService
+import spp.jetbrains.marker.jvm.psi.LoggerDetector
+import spp.jetbrains.marker.source.mark.api.MethodSourceMark
+import spp.jetbrains.marker.source.mark.api.event.SourceMarkEvent
+import spp.jetbrains.marker.source.mark.api.event.SourceMarkEventCode
+import spp.jetbrains.marker.source.mark.api.event.SourceMarkEventCode.MARK_USER_DATA_UPDATED
+import spp.jetbrains.marker.source.mark.guide.GuideMark
+import spp.jetbrains.marker.source.mark.inlay.config.InlayMarkVirtualText
+import spp.jetbrains.sourcemarker.SourceMarkerPlugin.vertx
+import spp.jetbrains.sourcemarker.mark.SourceMarkKeys
+import spp.plugin.registerIndicator
+import spp.protocol.SourceServices.Provide.toLiveViewSubscriberAddress
+import spp.protocol.artifact.ArtifactQualifiedName
+import spp.protocol.artifact.ArtifactType
+import spp.protocol.artifact.log.Log
+import spp.protocol.instrument.LiveSourceLocation
+import spp.protocol.view.LiveViewConfig
+import spp.protocol.view.LiveViewEvent
+import spp.protocol.view.LiveViewSubscription
+import java.awt.Color
+
+class LastLogIndicator : LiveIndicator() {
+    companion object {
+        private val log = org.slf4j.LoggerFactory.getLogger("spp.indicator.LastLogIndicator")
+        private val inlayForegroundColor = JBColor(Color.decode("#787878"), Color.decode("#787878"))
+    }
+
+    override val listenForEvents = listOf(MARK_USER_DATA_UPDATED)
+
+    override suspend fun trigger(guideMark: GuideMark, event: SourceMarkEvent) {
+        if (event.eventCode == MARK_USER_DATA_UPDATED && SourceMarkKeys.LOGGER_DETECTOR == event.params.firstOrNull()) {
+            val loggerDetector = event.params.get(1) as LoggerDetector
+            if (guideMark is MethodSourceMark) {
+                loggerDetector.getOrFindLoggerStatements(guideMark)
+            }
+            return
+        } else if (event.eventCode == MARK_USER_DATA_UPDATED && LoggerDetector.LOGGER_STATEMENTS != event.params.firstOrNull()) {
+            return //ignore other user data updates
+        }
+        val detectedLogPatterns = event.params.get(1) as List<LoggerDetector.DetectedLogger>
+
+        ApplicationManager.getApplication().runReadAction {
+            detectedLogPatterns.forEach {
+                addInlay(guideMark, it)
+            }
+        }
+    }
+
+    private fun addInlay(guideMark: GuideMark, detectedLog: LoggerDetector.DetectedLogger) {
+        val inlayMark = ArtifactCreationService.createExpressionInlayMark(
+            guideMark.sourceFileMarker,
+            detectedLog.lineLocation,
+            false
+        )
+        liveViewService.addLiveViewSubscription(
+            LiveViewSubscription(
+                null,
+                listOf(detectedLog.logPattern),
+                ArtifactQualifiedName(
+                    inlayMark.artifactQualifiedName.identifier,
+                    lineNumber = inlayMark.artifactQualifiedName.lineNumber,
+                    type = ArtifactType.EXPRESSION
+                ),
+                LiveSourceLocation(
+                    inlayMark.artifactQualifiedName.identifier,
+                    line = inlayMark.artifactQualifiedName.lineNumber!!
+                ),
+                LiveViewConfig("LOGS", listOf("endpoint_logs"))
+            )
+        ).onComplete {
+            if (it.succeeded()) {
+                val virtualText = InlayMarkVirtualText(inlayMark, " Waiting for logs...")
+                virtualText.useInlinePresentation = true
+                virtualText.textAttributes.foregroundColor = inlayForegroundColor
+                virtualText.fontSize = -0.5f
+                virtualText.relativeFontSize = true
+                inlayMark.configuration.activateOnMouseClick = false
+                inlayMark.configuration.virtualText = virtualText
+                inlayMark.apply(true)
+
+                val subscriptionId = it.result().subscriptionId!!
+                val consumer = vertx.eventBus().consumer<JsonObject>(toLiveViewSubscriberAddress(subscriptionId))
+                consumer.handler {
+                    val liveViewEvent = Json.decodeValue(it.body().toString(), LiveViewEvent::class.java)
+                    val rawMetrics = JsonObject(liveViewEvent.metricsData)
+                    val logData = Json.decodeValue(rawMetrics.getJsonObject("log").toString(), Log::class.java)
+                    virtualText.updateVirtualText(" [" + logData.arguments.joinToString(",") + "] @ " + logData.timestamp)
+                }
+                inlayMark.addEventListener { event ->
+                    if (event.eventCode == SourceMarkEventCode.MARK_REMOVED) {
+                        liveViewService.removeLiveViewSubscription(subscriptionId)
+                        consumer.unregister()
+                    }
+                }
+            } else {
+                log.error("Failed to add live view subscription", it.cause())
+            }
+        }
+    }
+}
+
+registerIndicator(LastLogIndicator())
